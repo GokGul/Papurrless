8b7bc3bcb38164fbd5eb98871ce3d291bdd0a135
Matthijs Reukers <thundertactics@gmail.com>
2016-03-21 21:56:55 +0100

    Layout van editor uitgewerkt, premium card verwijderd.

--- app/src/main/java/com/example/gokhan/papurrless/EditorActivity.java
+++ app/src/main/java/com/example/gokhan/papurrless/EditorActivity.java
@@ -2,6 +2,7 @@ package com.example.gokhan.papurrless;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class EditorActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class EditorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+        EditText productsEdit = (EditText) findViewById(R.id.editProducts);
+        EditText pricesEdit = (EditText) findViewById(R.id.editPrices);
+        productsEdit.setHorizontallyScrolling(true);
+        pricesEdit.setHorizontallyScrolling(true);
     }
 }
