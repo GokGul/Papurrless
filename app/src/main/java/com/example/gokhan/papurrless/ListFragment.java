b065da8b77732be60a8a16ac1acbe1f3b88c4cf6
gokguu <gokhan_g32@yahoo.com>
2016-03-22 17:57:32 +0100

    derpmarkt weg

--- app/src/main/java/com/example/gokhan/papurrless/ListFragment.java
+++ app/src/main/java/com/example/gokhan/papurrless/ListFragment.java
@@ -583,7 +583,7 @@ public class ListFragment extends Fragment {
             //add a certain number of receipts (use page to determine the range, or I guess the key would be better
             if (offset < 3) //this isn't relevant, just a useful limiter for the sake of testing longer lists
             {
-                moreReceipts.add(new ReceiptContent("DerpMarkt", "01-01-2009", "2x COCA-COLA\n2x APPELS\n1x DURR\n1x CUTOFF", "€2,33\n€3,75\n€11,22\n€13,77", "€100,00", true, 12));
+                //moreReceipts.add(new ReceiptContent("DerpMarkt", "01-01-2009", "2x COCA-COLA\n2x APPELS\n1x DURR\n1x CUTOFF", "€2,33\n€3,75\n€11,22\n€13,77", "€100,00", true, 12));
             }
             //normally you'd be pulling from the database at this point, note to only pull favorites
             adapter.addReceipts(moreReceipts);
@@ -696,7 +696,7 @@ public class ListFragment extends Fragment {
             //add a certain number of receipts (use page to determine the range, or I guess the key would be better
             if (offset < 13) //this isn't relevant, just a useful limiter for the sake of testing longer lists
             {
-                moreReceipts.add(new ReceiptContent("DerpMarkt", "01-01-2009", "2x COCA-COLA\n2x APPELS\n1x DURR\n1x CUTOFF", "€2,33\n€3,75\n€11,22\n€13,77", "€100,00", false, 12));
+                //moreReceipts.add(new ReceiptContent("DerpMarkt", "01-01-2009", "2x COCA-COLA\n2x APPELS\n1x DURR\n1x CUTOFF", "€2,33\n€3,75\n€11,22\n€13,77", "€100,00", false, 12));
             }
             //normally you'd be pulling from the database at this point
             adapter.addReceipts(moreReceipts);
